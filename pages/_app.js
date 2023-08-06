ababe91d1e516180995aceca356a72bc0e4a6c25
Manuel Munoz <manuel.munoz@whatnot.com>
2023-08-06 13:30:34 -0700

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,16 @@ import Navbar from "../components/navbar"
 import { Box, Flex } from "@chakra-ui/react"
 import styles from "../components/navbar.module.css"
 import "../styles/global.css"
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
     return (
         <ChakraProvider>
+            <Head>
+                <title>Manuel Munoz</title>
+                <meta property="og:title" content="Manuel Munoz" key="title"/>
+                <link rel="icon" href='/oskiHead.png'></link>
+            </Head>
             <Navbar />
             <Component {...pageProps} />
         </ChakraProvider>
